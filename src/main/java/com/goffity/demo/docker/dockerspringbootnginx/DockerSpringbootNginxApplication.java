e09c941965ae450698d75ed0f8dbc97b2b519b54
Narongwate Sangsakul <goffity@gmail.com>
2019-07-30 01:22:24 +0700

    init.

---
+++ src/main/java/com/goffity/demo/docker/dockerspringbootnginx/DockerSpringbootNginxApplication.java
@@ -0,0 +1,13 @@
+package com.goffity.demo.docker.dockerspringbootnginx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSpringbootNginxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerSpringbootNginxApplication.class, args);
+    }
+
+}
